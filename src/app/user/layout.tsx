e13ff10ef7babfde2cc7032dab322f45dada5cf6
Ravindra162 <tarakaravindra242005@gmail.com>
2024-07-04 12:55:46 +0530

    redux provider update

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,13 +1,13 @@
 "use client"; 
 
 import React from "react";
-import { Provider } from "react-redux";
-import { store } from "@/store/store";
+import ClientProvider from "@/components/ClientProvider";
 
-const ClientProvider = ({ children }: { children: React.ReactNode }) => {
+
+const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-      <Provider store={store}>{children}</Provider>
+      <ClientProvider>{children}</ClientProvider>
   );
 };
 
-export default ClientProvider;
+export default layout;
